8d5b17455ff7fa0bf7e527c08a70aaa9c7fe6802
Jacqueline Cameron <jskye429@mail.com>
2020-02-22 10:32:16 -0600

    added excel file for future mongo stuff

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav>
+    <navbar>
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/cart">Cart</Link>
       <Link to="/quote">Request a Quote</Link>
-    </nav>
+    </navbar>
   );
 }
 
